b25ab0728299adc15c6d33803ccb2fb73db0acfd
shunavpoudel22 <shunavpoudel22@gmail.com>
2026-02-11 17:35:02 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
-"""
-Main web application service. Serves the static frontend.
-"""
-
 from pathlib import Path
+
 import modal
-from .moshi import Moshi  # makes modal deploy also deploy moshi
 
 from .common import app
-
-static_path = Path(__file__).with_name("frontend").resolve()
-image = modal.Image.debian_slim(python_version="3.11").pip_install("fastapi==0.115.5")
-image = image.add_local_dir(static_path, "/assets")
+from .moshi import Moshi
 
 
-@app.function(
-    scaledown_window=600,
-    timeout=600,
-    image=image,
-)
+frontend = Path(__file__).with_name('frontend')
+image = modal.Image.debian_slim(python_version='3.11').pip_install('fastapi').add_local_dir(frontend, '/assets')
+
+
+@app.function(image=image, timeout=600)
 @modal.concurrent(max_inputs=100)
 @modal.asgi_app()
 def web():
@@ -25,19 +18,16 @@ def web():
     from fastapi.middleware.cors import CORSMiddleware
     from fastapi.staticfiles import StaticFiles
 
-    # disable caching on static files
-    StaticFiles.is_not_modified = lambda self, *args, **kwargs: False
+    webapp = FastAPI()
 
-    web_app = FastAPI()
-
-    web_app.add_middleware(
+    webapp.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+        allow_origins=['*'],
+        allow_methods=['*'],
+        allow_headers=['*'],
+        allow_credentials=True
     )
 
-    # Serve static files, for the frontend
-    web_app.mount("/", StaticFiles(directory="/assets", html=True))
-    return web_app
+    webapp.mount('/', StaticFiles(directory='/assets', html=True))
+
+    return webapp
